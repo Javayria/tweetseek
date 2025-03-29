d2c34001a90ef99ef72ebe6083aa60194b940049
Aansh Kotian <76598799+Aone52@users.noreply.github.com>
2025-03-29 17:46:43 -0400

    cleaned up endpoints, added response objects

--- backend/server.py
+++ backend/server.py
@@ -4,15 +4,15 @@ from google import genai
 
 
 from dotenv import load_dotenv
+
+from typing import Optional
+from dataclasses import (dataclass, asdict, field)
 from werkzeug.utils import secure_filename
-from utils import allowed_file
+from experts.geminiExpert import GeminiExpert
+from utils import (allowed_file, process_file)
 #for image processing
 from PIL import Image
 import os
-
-#for image processing
-import io
-
 
 #load env variables
 load_dotenv()
@@ -22,77 +22,53 @@ ALLOWED_EXTENSIONS = {"png","jpg","jpeg"}
 # Only run this block for Gemini Developer API
 # google's gemini client, globally configure api key
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+imageExpert = GeminiExpert(client=client)
 
 app = Flask(__name__)
 
-
-form_image = ""
 
 @app.route('/')
 def home():
     return "Hello tweetseek!"
 
-@app.route('/uploadimage',methods=['POST'])
-def imageContext():
-    # checks validity of uploading image file, saves it, uses it on form submission, on form cancellation the image will be cleared
 
-    global form_image 
+"""
+submit form endpoint will take a FormSubmissionRequest object and return a FormResponseObject
+"""
 
-    if "file" not in request.files:
-       return jsonify(success=False, message="No file part")
+@dataclass 
+class FormResponseObject:
+    GeminiResponse : Optional[str] = field(default = None)
+    AudioResponse: Optional[str] = field(default = None)
+    ContextualResponse: Optional[str] = field(default = None)
 
-    file = request.files["file"]
+@app.route('/submitform',methods=['POST'])
+def formSubmit():
+    #initally formResponseObject will have every expert's response as null
+    formResponse = FormResponseObject()
 
-    if file.filename == "":
-        return jsonify(success=False, message="No selected file")
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-
-        #essentially this code is used to process an uploaded image without saving it to a disk
-        # Read the file stream into a BytesIO object
-        file_stream = io.BytesIO(file.read())
-        #moves pointer back to start of the BytestIO stream
-        file_stream.seek(0)
-        #reads image data and prepares it as an image object
-        form_image = Image.open(file_stream)
-
-        return jsonify(
-            success=True,
-            message="File uploaded successful",
-            filename=filename,
-        )
-    return jsonify(success=False, message="File type not allowed")
-
-
-@app.route('/geminiexpert',methods=['GET'])
-def geminiExpert():
-
-    # https://ai.google.dev/gemini-api/docs/text-generation#python
-
-    global form_image
-
-    if form_image != "":
-        
-        #choose not to stream content back as styling for UI will be harder
-        response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=[form_image,"Only provide the bird's name, identify the bird from the provided photo"] #hardcoded to provide gemini with the prompt for identifying the image
-        )
-
-        print(response)
-
-        return jsonify(
-            success=True,
-            message= response.text
-        )
+    #checks for imageFile in request files
+    if "imageFile" not in request.files:
+       return jsonify(success=False, message="No image file included")
     
-    else:
-        return jsonify(
-            success=False,
-            message="image file not provided"
-        )
+    imageFile = request.files["imageFile"]
 
-# @app.route('/clearform',methods=['DELETE'])
+    if imageFile.filename == "":
+        return jsonify(success=False, message="No selected image file")
+    
+    if imageFile and allowed_file(imageFile.filename):
+        image = process_file(imageFile)
+        imageExpertResponse = imageExpert.analyze_image(image)
+    
+    if(imageExpertResponse):
+        formResponse.GeminiResponse = imageExpertResponse
+
+    return jsonify(
+        success = True,
+        message = "Form Submit Successful",
+        formResponse = asdict(formResponse)
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
