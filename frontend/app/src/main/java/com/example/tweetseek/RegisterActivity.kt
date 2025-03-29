f50a69e371690ffde53e3c7da9459adc459ce3df
Javayria <javayriamudassar@gmail.com>
2025-03-29 00:01:18 -0400

    removed unused imports from RegisterActivity.kt

--- frontend/app/src/main/java/com/example/tweetseek/RegisterActivity.kt
+++ frontend/app/src/main/java/com/example/tweetseek/RegisterActivity.kt
@@ -1,9 +1,7 @@
 package com.example.tweetseek
 import android.content.Intent
-import android.view.View
 import android.os.Bundle
 import android.widget.Toast
-import com.example.tweetseek.databinding.LoginPageBinding
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tweetseek.account.AccountManager
 import com.example.tweetseek.databinding.RegisterPageBinding
